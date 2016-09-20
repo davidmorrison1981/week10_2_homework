335fa8a33fc5fb7a8939dc829ebf83485d90d6a8
davidmorrison1981 <david.morrison@geminises.com>
2016-09-20 20:57:48 +0100

    test5 runs

--- shopping_basket/specs/basket_spec.js
+++ shopping_basket/specs/basket_spec.js
@@ -5,17 +5,17 @@ describe("shoppingBasket", function(){
 
   beforeEach(function(){
     basket.empty();
-    console.log(basket.contents);
+    // console.log(basket.contents);//included for debug
   });
 
   it("should be empty", function(){
-    console.log(basket.contents);
+    // console.log(basket.contents);//included for debug
     assert.equal(0, basket.contents.length);
   });
 
   it("should add items", function(){
     basket.addItems(),
-    console.log(basket.contents);
+    // console.log(basket.contents);//included for debug
     assert.notEqual(0, basket.contents.length)
   });
 
@@ -27,9 +27,18 @@ describe("shoppingBasket", function(){
 
   it("should remove items from basket", function(){
       basket.addItems(),
-      console.log(basket.contents);
+      //console.log(basket.contents);//included for debug
       toRemove = '{description: "potato", price: 1.00, includedInBogof: "N"}',
       basket.removeItems(),
       assert.equal(3, basket.contents.length)
   });
+
+  it("should empty items from basket", function(){
+      basket.addItems(),
+      //console.log(basket.contents);//included for debug
+      basket.empty(),
+      assert.equal(0, basket.contents.length)
+  });
+
+
 })
